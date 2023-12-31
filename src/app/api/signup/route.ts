2ac03289268528cab2d7a74d10f87fe738f440aa
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-12-31 18:18:32 +0530

    added api endpoints

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,26 @@
+import { connectToDb } from "@/utils";
+import prisma from "../../../../prisma";
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+export const POST = async (req: Request) => {
+  try {
+    const { name, email, password } = await req.json();
+
+    if(!name || !email || !password) return NextResponse.json({ error: "Missing required fields" }, { status: 422 });
+
+    await connectToDb();
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({ data: { name, email, password: hashedPassword } });
+
+    return NextResponse.json({ user }, { status: 201 });
+  }
+  catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+  finally {
+    await prisma.$disconnect();
+  }
+};
---
+++ src/app/api/users/route.ts
@@ -0,0 +1,17 @@
+import { connectToDb } from "@/utils";
+import prisma from "../../../../prisma";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request) => {
+  try {
+    await connectToDb();
+    const users = await prisma.user.findMany();
+
+    return NextResponse.json({ users }, { status: 200 });
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
